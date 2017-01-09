9e8287878573aa354001b900cc70b8bd5e55fb2b
tdlroberts <roberts.apskrums@testdevlab.com>
2017-01-09 22:26:18 +0200

    Had mistakes on previous version

--- features/screens/screen_intro.rb
+++ features/screens/screen_intro.rb
@@ -3,19 +3,20 @@ class ScreenIntro < ScreenBase
   def initialize(driver)
     @base = element(:id, 'content')
     #chose button id because it is the only unique value
-    @button_close_intro = element(:id, 'content')
+    @button_close_intro = element(:id, 'close_intro')
     #chose id, unique value
-    @imageView_intro_footer_image = element(:id, 'content')
+    @imageView_intro_footer_image = element(:id, 'intro_footer_image')
     #id, unique
-    @imageView_page_image = element(:id, 'content')
+    @imageView_page_image = element(:id, 'page_image')
     #id, unique
-    @textView_intro_text_header = element(:id, 'content')
+    @textView_intro_text_header = element(:id, 'intro_text_header')
     #found by id, unique value
-    @textView_intro_text_footer = element(:id, 'content')
+    @textView_intro_text_footer = element(:id, 'intro_text_footer')
     @driver = driver
   end
 
   def visible?
+    driver.find_element(:class_name, 'android.widget.Button').click()
     @driver.find_element(@base[:type], @base[:value])
     @driver.find_element(@button_close_intro[:type], @button_close_intro[:value])
     @driver.find_element(@imageView_intro_footer_image[:type], @imageView_intro_footer_image[:value])
